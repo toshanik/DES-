e374101be71e648883b35b841fac70e2264ee1cb
toshanik <62071098+toshanik@users.noreply.github.com>
2020-11-09 16:25:44 +0400

    Add files via upload

---
+++ DES/DES/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace DES
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonEncrypt = new System.Windows.Forms.Button();
+            this.buttonDecipher = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxEncodeKeyWord = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxDecodeKeyWord = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.buttonOpenFile = new System.Windows.Forms.Button();
+            this.buttonOpenFile2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonEncrypt
+            // 
+            this.buttonEncrypt.Location = new System.Drawing.Point(15, 64);
+            this.buttonEncrypt.Name = "buttonEncrypt";
+            this.buttonEncrypt.Size = new System.Drawing.Size(112, 23);
+            this.buttonEncrypt.TabIndex = 1;
+            this.buttonEncrypt.Text = "Зашифровать";
+            this.buttonEncrypt.UseVisualStyleBackColor = true;
+            this.buttonEncrypt.Click += new System.EventHandler(this.buttonEncrypt_Click);
+            // 
+            // buttonDecipher
+            // 
+            this.buttonDecipher.Location = new System.Drawing.Point(186, 64);
+            this.buttonDecipher.Name = "buttonDecipher";
+            this.buttonDecipher.Size = new System.Drawing.Size(112, 23);
+            this.buttonDecipher.TabIndex = 2;
+            this.buttonDecipher.Text = "Расшифровать";
+            this.buttonDecipher.UseVisualStyleBackColor = true;
+            this.buttonDecipher.Click += new System.EventHandler(this.buttonDecipher_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ключевое слово:";
+            // 
+            // textBoxEncodeKeyWord
+            // 
+            this.textBoxEncodeKeyWord.Location = new System.Drawing.Point(15, 38);
+            this.textBoxEncodeKeyWord.Name = "textBoxEncodeKeyWord";
+            this.textBoxEncodeKeyWord.Size = new System.Drawing.Size(112, 20);
+            this.textBoxEncodeKeyWord.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(183, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Ключевое слово:";
+            // 
+            // textBoxDecodeKeyWord
+            // 
+            this.textBoxDecodeKeyWord.Location = new System.Drawing.Point(186, 38);
+            this.textBoxDecodeKeyWord.Name = "textBoxDecodeKeyWord";
+            this.textBoxDecodeKeyWord.Size = new System.Drawing.Size(112, 20);
+            this.textBoxDecodeKeyWord.TabIndex = 6;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(16, 147);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(244, 126);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(266, 146);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(245, 127);
+            this.textBox2.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(517, 147);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(248, 127);
+            this.textBox3.TabIndex = 9;
+            // 
+            // buttonOpenFile
+            // 
+            this.buttonOpenFile.Location = new System.Drawing.Point(641, 279);
+            this.buttonOpenFile.Name = "buttonOpenFile";
+            this.buttonOpenFile.Size = new System.Drawing.Size(124, 23);
+            this.buttonOpenFile.TabIndex = 10;
+            this.buttonOpenFile.Text = "Открыть в блокноте";
+            this.buttonOpenFile.UseVisualStyleBackColor = true;
+            this.buttonOpenFile.Click += new System.EventHandler(this.buttonOpenFile_Click);
+            // 
+            // buttonOpenFile2
+            // 
+            this.buttonOpenFile2.Location = new System.Drawing.Point(388, 279);
+            this.buttonOpenFile2.Name = "buttonOpenFile2";
+            this.buttonOpenFile2.Size = new System.Drawing.Size(123, 23);
+            this.buttonOpenFile2.TabIndex = 11;
+            this.buttonOpenFile2.Text = "Открыть в блокноте";
+            this.buttonOpenFile2.UseVisualStyleBackColor = true;
+            this.buttonOpenFile2.Click += new System.EventHandler(this.buttonOpenFile2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Исходный текст";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(263, 117);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Зашифрованный";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(514, 117);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(98, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Расшифрованный";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(814, 326);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.buttonOpenFile2);
+            this.Controls.Add(this.buttonOpenFile);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBoxDecodeKeyWord);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxEncodeKeyWord);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonDecipher);
+            this.Controls.Add(this.buttonEncrypt);
+            this.Name = "Form1";
+            this.Text = "Американский алгоритм шифрования DES";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonEncrypt;
+        private System.Windows.Forms.Button buttonDecipher;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxEncodeKeyWord;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxDecodeKeyWord;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button buttonOpenFile;
+        private System.Windows.Forms.Button buttonOpenFile2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
